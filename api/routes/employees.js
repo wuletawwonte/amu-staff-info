22c7a682fdcec71ff976943c274eedd720b03861
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-08-06 19:04:33 +0300

    mysql node package successfully gets data from database

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -1,10 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mysql = require("mysql");
 
-router.get('/', (req, res, next) => {
+var connection = mysql.createConnection({
+  host: "10.144.12.200",
+  user: "wuletaw",
+  password: "Phantome@81",
+  database: "amu_employees",
+});
+
+router.get("/", (req, res, next) => {
+  connection.connect();
+
+  connection.query("SELECT * FROM employees", function (err, rows, fields) {
+    if (err) throw err;
+    console.log("The solution is: ", rows);
     res.status(200).json({
-        message: "employee service works"
+        'data': rows
     });
+  });
+
+  connection.end();
 });
 
 module.exports = router;
