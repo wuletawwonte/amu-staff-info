f518cbc4df2602aa0401bb68278477931baf1f2a
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-08-07 10:00:43 +0300

    nodemon added

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 const employeeRoutes = require('./api/routes/employees');
 
+// a middleware for passing application/json
+app.use(express.json());
+
+// for parsing application/x-www-form-urlencodedapp.use(express.urlencoded({extended: true}));
 app.use('/employee', employeeRoutes);
 
 module.exports = app;
