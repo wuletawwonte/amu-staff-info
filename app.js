6e57b17b210170eb57a4c4d3faf49c7fe134337b
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-08-07 16:09:25 +0300

    error handling done for unhandled requests

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const employeeRoutes = require('./api/routes/employees');
+const employeeRoutes = require("./api/routes/employees");
 
 // a middleware for passing application/json
 app.use(express.json());
 
 // for parsing application/x-www-form-urlencodedapp.use(express.urlencoded({extended: true}));
-app.use('/employee', employeeRoutes);
+app.use("/employee", employeeRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Service Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+  });
+});
 
 module.exports = app;
-
